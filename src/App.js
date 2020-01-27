08e117a4b5da07ef3c8e220762864a6d9a0880c9
sserges <agounkeserge@gmail.com>
2020-01-27 12:14:49 +0000

    Create the header

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+// CSS
+import './App.css'
+
+import Header from './components/Header';
+
+class App extends Component {
+  state = {
+    pseudo: this.props.match.params.pseudo
+  }
+
+  render () {
+    return (
+      <div className='box'>
+        <Header pseudo={this.state.pseudo}/>
+        <div className='cards'>
+          <div className='card'>
+            <h2>Une Carte</h2>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
