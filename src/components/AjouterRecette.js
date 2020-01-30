c0cb3f7d6c8d82bab0c3baf23afda3b1fd2a32b1
sserges <agounkeserge@gmail.com>
2020-01-30 11:40:09 +0000

    Form with React

--- src/components/AjouterRecette.js
+++ src/components/AjouterRecette.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
 
 class AjouterRecette extends Component {
+
+    state = {
+        nom: '',
+        image: '',
+        ingredients: '',
+        instructions: ''
+    }
+
+    handleChange = event => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    }
+
     render() {
         return (
-            <div>
-                <h2>Ajouter Recette</h2>
+            <div className="card">
+                <form className="admin-form ajouter-recette">
+                    <input value={this.state.nom} onChange={this.handleChange} name="nom" type="text" placeholder='Nom de la recette' />
+                    <input value={this.state.image} onChange={this.handleChange} name="image" type="text" placeholder="Nom de l'image" />
+                    <textarea value={this.state.ingredients} onChange={this.handleChange} name="ingredients" rows="3" placeholder='Liste des ingrédients' />
+                    <textarea value={this.state.instructions} onChange={this.handleChange} name="instructions" rows="15" placeholder='Liste des instructions' />
+                </form>
             </div>
         )
     }
