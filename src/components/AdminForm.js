c6bce596a7a2e31b8d84d4248cf22792e2687091
sserges <agounkeserge@gmail.com>
2020-02-06 16:19:06 +0000

    Manage the modification form

--- src/components/AdminForm.js
+++ src/components/AdminForm.js
@@ -1,13 +1,46 @@
 import React from 'react';
 
-const AdminForm = () => {
+const AdminForm = ({id: key, majRecette, recettes}) => {
+    const recette = recettes[key];
+
+    const handleChange = (event, key) => {
+        const { name, value } = event.target;
+        const recette = recettes[key];
+        recette[name] = value;
+        majRecette(key, recette);
+    };
+
     return (
         <div className="card">
             <form className="admin-form">
-                <input name="nom" type="text" placeholder='Nom de la recette' />
-                <input name="image" type="text" placeholder="Nom de l'image" />
-                <textarea name="ingredients" rows="3" placeholder='Liste des ingrédients' />
-                <textarea name="instructions" rows="15" placeholder='Liste des instructions' />
+                <input 
+                    value={recette.nom} 
+                    onChange={e => handleChange(e, key)} 
+                    name="nom" 
+                    type="text" 
+                    placeholder='Nom de la recette' 
+                />
+                <input 
+                    value={recette.image} 
+                    onChange={e => handleChange(e, key)} 
+                    name="image" 
+                    type="text" 
+                    placeholder="Nom de l'image" 
+                />
+                <textarea 
+                    value={recette.ingredients} 
+                    onChange={e => handleChange(e, key)} 
+                    name="ingredients" 
+                    rows="3" 
+                    placeholder='Liste des ingrédients' 
+                />
+                <textarea 
+                    value={recette.instructions} 
+                    onChange={e => handleChange(e, key)} 
+                    name="instructions" 
+                    rows="15" 
+                    placeholder='Liste des instructions' 
+                />
                 
             </form>
             <button>Supprimer</button>
